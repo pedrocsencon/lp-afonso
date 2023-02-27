aee2a6d8bd752bf0a51cb3c405d48b8d9f44bb05
Pedro Lucas Copatti <pedro.copatti.sencon@tc.com.br>
2023-02-27 16:27:53 -0300

    text

--- src/pages/obrigado.tsx
+++ src/pages/obrigado.tsx
@@ -10,30 +10,30 @@ export default function Obrigado() {
 
     return (
         <Stack textAlign='center' spacing={6} paddingY={12} id='background' alignItems='center' backgroundImage="url('../assets/backgrond.png')" width='100%' paddingX={isLargerThan900 ? 24 : 4} backgroundColor={'white'}>
-            <Text>Falta só uma coisa!</Text>
+            <Text fontSize='lg' fontWeight='bold'>Falta só uma coisa!</Text>
             <Flex width={['95%', '70%', '50%']} paddingY={2} justifyContent='center' alignItems='center' background='orange.500' borderRadius={8}>
                 <RiAlertFill size={18} color='white' />
                 <Text marginLeft={2} fontWeight='bold' fontSize={['xs', 'md', 'md']} textColor='white'>
                     SUA INSCRIÇÃO AINDA NÃO FOI CONFIRMADA
                 </Text>
             </Flex>
-            <Text>Para garantir sua vaga, basta seguir as instruções abaixo:</Text>
+            <Text fontSize='lg'>Para garantir sua vaga, basta seguir as instruções abaixo:</Text>
             <Box>
                 <Image alt="alert" width={60} src={alertIcon} />
             </Box>
-            <Text>
+            <Text fontSize='lg'>
                 Em breve você receberá um e-mail, para confirmar <strong>é só abrir e clicar no link que te enviei</strong>.
             </Text>
             <Box>
                 <Image alt="email" width={60} src={emailIcon} />
             </Box>
-            <Text>
+            <Text fontSize='lg'>
                 A nossa conversa vai acontecer por e-mail, é por lá que te mandarei tudo que você precisa saber sobre o nosso desafio.
             </Text>
             <Text fontSize='2xl' fontWeight='bold'>
                 Mas não é só isso!
             </Text>
-            <Text>
+            <Text fontSize='lg'>
                 Já no meu primeiro e-mail eu tenho uma surpresa para você, fique ligado.
             </Text>
             <Stack padding={8} alignItems='center' width={['95%', '70%', '50%']} textAlign='center' backgroundColor='orange.500' borderRadius={12}>
